77d804d2b575a7991b9025a43e8fad58ded2d31d
Daria Malenko <malienko.99@ukr.net>
2021-11-03 21:34:12 +0200

    #5 Templates and i18n: Product demo message block is added

--- app/code/Dariam/Catalog/registration.php
+++ app/code/Dariam/Catalog/registration.php
@@ -1,9 +1,8 @@
 <?php
-
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
-    'Dariam_Cms',
+    'Dariam_Catalog',
     __DIR__
 );
